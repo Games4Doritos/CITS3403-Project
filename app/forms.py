bb96c9fb1049e6c3e74eb64a5420ea83a7878aba
Siyu Zhu <lilyzhusiyu@gmail.com>
2026-05-05 20:17:16 +0800

    Add some invalid input  messages for display

--- app/forms.py
+++ app/forms.py
@@ -6,16 +6,16 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo
 from app.models import Player
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired('Required'), Email()])# Error messages design later
+    password = PasswordField('Password', validators=[DataRequired('Required')])
 
 class SignupForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    email = StringField('Email', validators=[DataRequired('Required'), Email(message='Invalid email format')])
+    password = PasswordField('Password', validators=[DataRequired('Required'), Length(min=6, message='At least 6 characters')])
     confirm_password = PasswordField(
         'Confirm Password', 
         validators=[
-            DataRequired(), 
+            DataRequired('Required'), 
             EqualTo('password', message='Confirmation password does not match')
         ]
     )
