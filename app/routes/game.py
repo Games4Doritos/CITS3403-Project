69b61189d1253ab065a4be59a6a9c36d9876230b
EvanCaraiscos <evancar1234@gmail.com>
2026-05-16 12:56:47 +0800

    Simplify game route - inject debuff via Jinja, remove Sabotage model references

--- app/routes/game.py
+++ app/routes/game.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request
 from flask_login import current_user
 from json import loads
 from app import db
-from app.models import BestStats, Sabotage
+from app.models import BestStats
 
 game = Blueprint("game", __name__)
+
+SABOTAGE_DEBUFF = 0.5
 
 @game.route('/play', methods=["GET", "POST"])
 def play():
@@ -43,14 +45,6 @@ def play():
             valid = validateResults(requestData)
             if not valid:
                 return "Invalid Run Results", 400
-            
-            # Deactivate any active sabotage after run ends
-            active_sabotage = Sabotage.query.filter_by(
-                target_id=current_user.id,
-                active=True
-            ).first()
-            if active_sabotage:
-                active_sabotage.active = False
 
             if not current_user.best_stats:
                 bestStats = BestStats(
@@ -66,31 +60,24 @@ def play():
             else:
                 curBestStats = current_user.best_stats
                 current_user.best_stats.total_games += 1
+
                 if curBestStats.highscore < requestData["totalScore"]:
                     current_user.best_stats.highscore = round(requestData["totalScore"])
                 if curBestStats.longest_time < requestData["finalTime"]:
                     current_user.best_stats.longest_time = requestData["finalTime"]
                 current_user.best_stats.currency += int(requestData["newCurrency"])
                 current_user.best_stats.jump_count += int(requestData["totalJumps"])
+                
+                # Deactivate debuff after run ends
+                current_user.best_stats.debuffed = False
                 db.session.commit()
 
             return "Run Successfully Submitted!", 200
     
-    return render_template('play.html')
+    # Inject debuff into template for Jinja
+    debuff = 0
+    if current_user.is_authenticated and current_user.best_stats:
+        if current_user.best_stats.debuffed:
+            debuff = SABOTAGE_DEBUFF
 
-
-@game.route('/play/sabotage-status', methods=["GET"])
-def get_sabotage_status():
-    # Check if current player has an active sabotage
-    if current_user.is_authenticated:
-        active_sabotage = Sabotage.query.filter_by(
-            target_id=current_user.id,
-            active=True
-        ).first()
-        if active_sabotage:
-            return jsonify({
-                'sabotaged': True,
-                'debuff': active_sabotage.multiplier_debuff
-            }), 200
-    
-    return jsonify({'sabotaged': False, 'debuff': 0}), 200
+    return render_template('play.html', debuff=debuff)
