70296504cb1954a456103a9795e61f2891c11d9e
Siyu Zhu <lilyzhusiyu@gmail.com>
2026-05-16 12:59:16 +0800

    retain the booleans in config.py

--- app/config.py
+++ app/config.py
@@ -21,8 +21,8 @@ class Config:
     # Gmail SMTP server with TLS encryption
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT"))
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") == "True"
-    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL") == "True"
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_USERNAME")
